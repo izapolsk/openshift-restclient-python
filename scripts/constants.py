690a823f3783fd084c462754032f17d4c2f55c64
Eugene Zapolsky <izapolsk@redhat.com>
2017-12-10 11:32:57 +0200

    Update constants.py

--- scripts/constants.py
+++ scripts/constants.py
@@ -19,7 +19,7 @@ KUBERNETES_BRANCH = "master"
 
 # Spec version will be set in downloaded spec and all
 # generated code will refer to it.
-SPEC_VERSION = "v3.7.0"
+SPEC_VERSION = "v3.5.0"
 
 # client version for packaging and releasing. It can
 # be different than SPEC_VERSION.
